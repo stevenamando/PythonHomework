5f53ee84a2e003d3556c99ac93f731b4c282f2bb
Steven Barnas <stevenamando94@gmail.com>
2019-09-10 11:53:10 -0500

    Added NumberInformation homework

---
+++ NumberInformation.java
@@ -0,0 +1,31 @@
+// Number Information Homework - Steven Barnas
+import java.util.Scanner;
+
+public class NumberInformation {
+   public static void main(String[] args) {
+   Scanner keyboard = new Scanner(System.in);
+   System.out.print("What is your favorite integer? ");
+   int x = keyboard.nextInt();
+   System.out.println("You entered " + x + " as your number");
+      if(x == 42){
+         System.out.println("I like the number 42 also!");
+      }         
+      
+      if(x%2 == 0){
+         System.out.println("Your number is even.");
+      } 
+      else {
+         System.out.println("Your number is odd.");
+      }      
+     
+      if(x > 0){
+         System.out.println("Your number is positive.");
+      } 
+      else if(x < 0){
+         System.out.println("Your number is negative.");
+      }
+      else{
+         System.out.println("Your number is zero.");
+      }         
+   }
+}
